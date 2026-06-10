248c7efb63769f10556232b15df3a07e4a14ac18
manupor <135657852+manupor@users.noreply.github.com>
2026-06-09 23:53:54 -0600

    fix: remove conflicting inline style, fix parallax mobile with correct absolute positioning

--- src/components/homes/home-1/ParallaxImgHomeOne.tsx
+++ src/components/homes/home-1/ParallaxImgHomeOne.tsx
@@ -1,19 +1,6 @@
 const ParallaxImgHomeOne = () => {
 	return (
 		<>
-			<style>{`
-				@media (max-width: 767px) {
-					.parallax-image {
-						height: auto !important;
-					}
-					.parallax-image .background-image {
-						position: relative !important;
-						height: 100vw !important;
-						background-size: cover !important;
-						background-position: center center !important;
-					}
-				}
-			`}</style>
 			<div className="parallax-image">
 				<div
 					className="background-image"
