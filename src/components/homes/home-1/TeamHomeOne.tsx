6e10f7e7551958214d3813e4df8a44f8bc6c26de
manupor <135657852+manupor@users.noreply.github.com>
2026-06-21 22:03:40 -0600

    fix: show full portrait in team card using object-fit contain

---
+++ src/components/homes/home-1/TeamHomeOne.tsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { useLang } from "../../../context/LanguageContext";
+
+const TeamHomeOne = () => {
+  const { t } = useLang();
+  return (
+    <>
+      <section className="team-section-three text-center">
+        <div className="outer-box">
+          <div className="main-title-two">
+            <span className="sub-title">{t("teamhome.tag")}</span>
+            <h2>{t("teamhome.title")}</h2>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-lg-4 col-md-6 col-sm-12 block-column">
+              <div className="team-block-three">
+                <figure className="image"><Link to="/"><img src="/photo%202.jpeg" alt="Manu Portuguez" style={{objectFit: "contain", objectPosition: "center top", background: "#0d0d0d"}} /></Link></figure>
+                <div className="text-box">
+                  <h3><Link to="/">Manu Portuguez</Link></h3>
+                  <span className="designation">{t("teamhome.role")}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TeamHomeOne;
